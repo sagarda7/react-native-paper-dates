71019ad4cb28af2877a2556fc544f41e527ab1dc
Narayan Thapa <dpkthapa15@example.com>
2025-02-28 16:43:59 +0545

    test push by collab

--- src/Date/DatePickerModalHeader.tsx
+++ src/Date/DatePickerModalHeader.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   },
   appbarHeader: {
     elevation: 0,
-    backgroundColor: '#dadaff', //custom by fieldEx
+    backgroundColor: '#dadaff', //custom by fieldEx ddddd
   },
   buttonStyle: {
     paddingHorizontal: 8,
